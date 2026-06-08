22765b7990e0d03bd6c3fbd8f538d33d3e479c77
huhansibuxin <huhansibuxin@users.noreply.github.com>
2026-06-08 14:22:07 +0800

    fix: restore AppDelegate.m overwritten by control file content

--- NoiseCancelToggleApp/AppDelegate.m
+++ NoiseCancelToggleApp/AppDelegate.m
@@ -1,9 +1,38 @@
-Package: com.huhansibuxin.noisecanceltoggle
-Name: 降噪切换
-Version: 1.0.4
-Description: 桌面一键切换 AirPods 降噪/通透模式。点击桌面图标即可在降噪和通透模式之间切换，无需进入程序。
-Section: Tweaks
-Depends: ellekit, firmware (>= 16.0)
-Architecture: iphoneos-arm64e
-Author: huhansibuxin
-Maintainer: huhansibuxin
+#import <UIKit/UIKit.h>
+
+@interface BluetoothDevice : NSObject
+- (BOOL)isAppleAudioDevice;
+- (unsigned int)listeningMode;
+- (BOOL)setListeningMode:(unsigned int)arg1;
+@end
+
+@interface BluetoothManager : NSObject
++ (id)sharedInstance;
+- (id)connectedDevices;
+@end
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    /* Fallback: if the tweak fails to intercept, the app itself toggles ANC and exits */
+    BluetoothManager *btMgr = [NSClassFromString(@"BluetoothManager") sharedInstance];
+    if (btMgr) {
+        NSArray *devices = [btMgr connectedDevices];
+        for (id dev in devices) {
+            if ([dev isAppleAudioDevice]) {
+                unsigned int current = [dev listeningMode];
+                unsigned int next = (current == 1) ? 2 : 1;
+                [dev setListeningMode:next];
+                break;
+            }
+        }
+    }
+    exit(0);
+    return YES;
+}
+
+@end
